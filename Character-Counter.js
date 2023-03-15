acce02b65f33749888b8eeee58fe06bd63454263
AVS <123315354+AVS-Memer@users.noreply.github.com>
2023-03-15 08:28:03 -0400

    Update Character-Counter.js

--- Character-Counter.js
+++ Character-Counter.js
@@ -1,2 +1,1 @@
-let string = prompt("What is the string you want to measure the length of?");
-alert("The length of the string is " + string.length);
+let string = prompt("What is the string you want to measure the length of?"); alert("The length of the string is " + string.length);
